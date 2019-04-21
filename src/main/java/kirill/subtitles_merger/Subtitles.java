9ffc1fdbfed8ac4a9127dab660d99f759ab4b08d
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-04-21 18:26:51 +0300

    fix & test

--- src/main/java/kirill/subtitles_merger/Subtitles.java
+++ src/main/java/kirill/subtitles_merger/Subtitles.java
@@ -16,7 +16,9 @@ public class Subtitles {
     public String toString() {
         StringBuilder result = new StringBuilder();
 
-        for (SubtitlesElement subtitlesElement : elements) {
+        for (int i = 0; i < elements.size(); i++) {
+            SubtitlesElement subtitlesElement = elements.get(i);
+
             result.append(subtitlesElement.getNumber());
             result.append("\n");
 
@@ -25,12 +27,19 @@ public class Subtitles {
             result.append(DateTimeFormat.forPattern("HH:mm:ss,SSS").print(subtitlesElement.getTo()));
             result.append("\n");
 
-            for (SubtitlesElementLine line : subtitlesElement.getLines()) {
+            for (int j = 0; j < subtitlesElement.getLines().size(); j++) {
+                SubtitlesElementLine line = subtitlesElement.getLines().get(j);
+
                 result.append(line.getText());
-                result.append("\n");
+
+                if (j != subtitlesElement.getLines().size() - 1 || i != elements.size() - 1) {
+                    result.append("\n");
+                }
             }
 
-            result.append("\n");
+            if (i != elements.size() - 1) {
+                result.append("\n");
+            }
         }
 
         return result.toString();
