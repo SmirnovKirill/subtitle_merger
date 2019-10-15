24496a16b7c23dabae0503b7142c0e7daf650ade
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-10-15 13:27:28 +0300

    keep working

---
+++ src/main/java/kirill/subtitles_merger/Config.java
@@ -0,0 +1,17 @@
+package kirill.subtitles_merger;
+
+import com.neovisionaries.i18n.LanguageAlpha3Code;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Config {
+    private String ffmpegPath;
+
+    private String ffprobePath;
+
+    private LanguageAlpha3Code upperLanguage;
+
+    private LanguageAlpha3Code lowerLanguage;
+}
--- src/main/java/kirill/subtitles_merger/Main.java
+++ src/main/java/kirill/subtitles_merger/Main.java
@@ -1,5 +1,6 @@
 package kirill.subtitles_merger;
 
+import com.neovisionaries.i18n.LanguageAlpha3Code;
 import kirill.subtitles_merger.ffprobe.Ffprobe;
 import kirill.subtitles_merger.ffprobe.FfprobeSubtitlesInfo;
 import lombok.extern.apachecommons.CommonsLog;
@@ -9,10 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.prefs.Preferences;
 
 @CommonsLog
@@ -25,27 +23,111 @@ public class Main {
     );
 
     public static void main(String[] args) throws IOException {
-        Preferences preferences = Preferences.userRoot().node(PREFERENCES_ROOT_NODE);
-        String ffmpegPath = preferences.get("ffmpeg_path", "");
-        if (StringUtils.isBlank(ffmpegPath)) {
-            preferences.put("ffmpeg_path", "/usr/bin/ffmpeg");
+        Scanner scanner = new Scanner(System.in);
+
+        Config config = getConfig(scanner);
+        File directoryWithVideos = getDirectoryWithVideos(scanner);
+
+        List<File> videoFiles = getVideoFiles(directoryWithVideos);
+        if (CollectionUtils.isEmpty(videoFiles)) {
+            log.info("no videos in the provided directory");
+            return;
         }
 
-        String ffprobePath = preferences.get("ffprobe_path", "");
-        if (StringUtils.isBlank(ffprobePath)) {
-            preferences.put("ffprobe_path", "/usr/bin/ffprobe");
-        }
-        Ffprobe fFprobe = new Ffprobe(ffprobePath);
-
-
-        List<File> videoFiles = getVideoFiles(new File("/home/user"));
-
+        Ffprobe fFprobe = new Ffprobe(config.getFfprobePath());
         for (File file : videoFiles) {
             FfprobeSubtitlesInfo subtitlesInfo = fFprobe.getSubtitlesInfo(file);
             if (!CollectionUtils.isEmpty(subtitlesInfo.getSubtitleStreams())) {
                 log.info("file " + file.getAbsolutePath() + " has " + subtitlesInfo.getSubtitleStreams().size() + " subs");
             }
         }
+    }
+
+    private static Config getConfig(Scanner scanner) {
+        Preferences preferences = Preferences.userRoot().node(PREFERENCES_ROOT_NODE);
+
+        boolean hasErrors;
+
+        String ffmpegPath = preferences.get("ffmpeg_path", "");
+        if (StringUtils.isBlank(ffmpegPath)) {
+            System.out.println("please provide path to the ffmpeg executable file");
+            ffmpegPath = scanner.next();
+            //todo валидация
+        }
+
+        String ffprobePath = preferences.get("ffprobe_path", "");
+        if (StringUtils.isBlank(ffprobePath)) {
+            System.out.println("please provide path to the ffprobe executable file");
+            ffprobePath = scanner.next();
+            //todo валидация
+        }
+
+        String upperLanguage = preferences.get("upper_language", "");
+        hasErrors = false;
+        while (StringUtils.isBlank(upperLanguage) || hasErrors) {
+            if (!hasErrors) {
+                System.out.println("please provide preferred language (ISO 639-2) to upper subtitles");
+            }
+
+            upperLanguage = scanner.next();
+            if (LanguageAlpha3Code.getByCodeIgnoreCase(upperLanguage) == null) {
+                hasErrors = true;
+                System.out.println("incorrect language format");
+            } else {
+                hasErrors = false;
+            }
+        }
+
+        String lowerLanguage = preferences.get("lowe_language", "");
+        hasErrors = false;
+        while (StringUtils.isBlank(lowerLanguage) || hasErrors) {
+            if (!hasErrors) {
+                System.out.println("please provide preferred language (ISO 639-2) to lower subtitles");
+            }
+
+            lowerLanguage = scanner.next();
+            if (LanguageAlpha3Code.getByCodeIgnoreCase(upperLanguage) == null) {
+                hasErrors = true;
+                System.out.println("incorrect language format");
+            } else if (lowerLanguage.equalsIgnoreCase(upperLanguage)) {
+                System.out.println("languages have to be different!");
+                hasErrors = true;
+            } else {
+                hasErrors = false;
+            }
+        }
+
+        //todo валидация того что достали из preferences
+        return new Config(
+                ffmpegPath,
+                ffprobePath,
+                LanguageAlpha3Code.getByCodeIgnoreCase(upperLanguage),
+                LanguageAlpha3Code.getByCodeIgnoreCase(lowerLanguage)
+        );
+    }
+
+    private static File getDirectoryWithVideos(Scanner scanner) {
+        String path = null;
+        boolean hasErrors = false;
+
+        while (StringUtils.isBlank(path) || hasErrors) {
+            if (!hasErrors) {
+                System.out.println("please provide full path to the directory with video files");
+            }
+
+            path = scanner.next();
+            if (!new File(path).exists()) {
+                hasErrors = true;
+                System.out.println("directory does not exist");
+            } else if (!new File(path).isDirectory()) {
+                hasErrors = true;
+                System.out.println("specified path contains a file not a directory");
+            } else {
+                hasErrors = false;
+            }
+        }
+
+        return new File(path);
     }
 
     private static List<File> getVideoFiles(File folder) {
