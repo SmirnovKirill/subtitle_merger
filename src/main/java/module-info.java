e8aaa0f1c51551bbe5f44062fd40666f03eb6fc4
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-11-08 23:46:46 +0300

    modularize

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module subtitles.merger {
+    requires static lombok;
+
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires commons.logging;
+    requires nv.i18n;
+    requires org.apache.commons.collections4;
+    requires org.apache.commons.io;
+    requires org.apache.commons.lang3;
+    requires org.joda.time;
+}
