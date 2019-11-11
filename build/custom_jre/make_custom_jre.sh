a67aa1886c97a38e29d23b31c6b8dc20dc49f09a
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-11-11 23:05:51 +0300

    build

---
+++ build/custom_jre/make_custom_jre.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function make_custom_jre {
+  rm $1 -rf
+
+  jlink --module-path ../manual_downloads/jdk/$1/jmods \
+    --add-modules java.base,java.prefs \
+    --output $1
+
+  echo "*" >> $1/.gitignore
+}
+
+make_custom_jre "win_64"
+make_custom_jre "linux_64"
