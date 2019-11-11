c32af3366076d32052732b78c97c0ca4aebcd6d7
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-11-11 23:21:37 +0300

    build

--- build/custom_jre/make_custom_jre.sh
+++ build/custom_jre/make_custom_jre.sh
@@ -4,6 +4,8 @@ function make_custom_jre {
   rm $1 -rf
 
   jlink --module-path ../manual_downloads/jdk/$1/jmods \
+    --compress=2 \
+    --no-man-pages \
     --add-modules java.base,java.prefs \
     --output $1
 
