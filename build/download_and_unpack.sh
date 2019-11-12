3941f854c2b86c2cbd1da572c0eba091bc8210e2
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-11-12 23:55:37 +0300

    keep writing scripts

--- build/download_and_unpack.sh
+++ build/download_and_unpack.sh
@@ -12,5 +12,7 @@ if [[ $BUILD_LINUX_64 == "y" ]]; then
     rm $CURRENT_DIRECTORY/downloads/jdk/linux_64/* -rf
     wget -P $CURRENT_DIRECTORY/downloads/jdk/linux_64/ $JDK_LINUX_64_DOWNLOAD_URL
     create_git_ignore $CURRENT_DIRECTORY/downloads/jdk/linux_64/.gitignore
+    tar -xzf $CURRENT_DIRECTORY/downloads/jdk/linux_64/*.gz --strip 1 -C $CURRENT_DIRECTORY/downloads/jdk/linux_64
+    rm $CURRENT_DIRECTORY/downloads/jdk/linux_64/*.gz
   fi
 fi
