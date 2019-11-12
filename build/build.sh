4a943d7ab1bac344cd24efbec9b50787e1f25669
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-11-12 22:29:03 +0300

    keep writing scripts

--- build/build.sh
+++ build/build.sh
@@ -4,5 +4,12 @@ set -e
 
 CURRENT_DIRECTORY="$(dirname "$0")"
 
+function create_git_ignore {
+  cat << EndOfText | head -c -1 > $1
+*
+!.gitignore
+EndOfText
+}
+
 source "$CURRENT_DIRECTORY/settings.sh"
 source "$CURRENT_DIRECTORY/download_and_unpack.sh"
--- build/download_and_unpack.sh
+++ build/download_and_unpack.sh
@@ -7,4 +7,10 @@ if [[ $BUILD_LINUX_64 == "y" ]]; then
   elif [[ -z "$(ls $CURRENT_DIRECTORY/downloads/jdk/linux_64)" ]]; then
     DOWNLOAD_JDK="y"
   fi
+
+  if [[ $DOWNLOAD_JDK == "y" ]]; then
+    rm $CURRENT_DIRECTORY/downloads/jdk/linux_64/* -rf
+    wget -P $CURRENT_DIRECTORY/downloads/jdk/linux_64/ $JDK_LINUX_64_DOWNLOAD_URL
+    create_git_ignore $CURRENT_DIRECTORY/downloads/jdk/linux_64/.gitignore
+  fi
 fi
