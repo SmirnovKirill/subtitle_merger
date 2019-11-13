35aa96fc16cb185f9c7c949a54395814c1f42f77
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-11-13 19:44:09 +0300

    keep writing scripts

--- build/download_and_unpack.sh
+++ build/download_and_unpack.sh
@@ -16,10 +16,10 @@ function download_and_unpack {
 
   wget -P "$1" "$2"
 
-  if [[ -z $(find "$1" -maxdepth 1 -name "*.gz") ]]; then
+  if [[ -n $(find "$1" -maxdepth 1 -name "*.gz") ]]; then
     bsdtar --strip-components=1 -xzf "${1:?}/*.gz" -C "$1"
     rm "{$1:?}/*.gz"
-  elif [[ -z $(find "$1" -maxdepth 1 -name "*.zip") ]]; then
+  elif [[ -n $(find "$1" -maxdepth 1 -name "*.zip") ]]; then
     bsdtar --strip-components=1 -xzf "${1:?}/*.zip" -C "$1"
     rm "{$1:?}/*.zip"
   else
@@ -35,6 +35,24 @@ if [[ $BUILD_LINUX_64 == "y" ]]; then
   else
     echo "jdk for linux x64 has already been downloaded"
   fi
+
+  DOWNLOAD_JAVAFX=$(need_to_download "$CURRENT_DIRECTORY/downloads/javafx/linux")
+  if [[ $DOWNLOAD_JAVAFX == "y" ]]; then
+    download_and_unpack "$CURRENT_DIRECTORY/downloads/javafx/linux" "$OPENJFX_LINUX_DOWNLOAD_URL"
+  else
+    echo "javafx for linux has already been downloaded"
+  fi
+
+  if [[ $INCLUDE_FFMPEG == "y" ]]; then
+    DOWNLOAD_FFMPEG=$(need_to_download "$CURRENT_DIRECTORY/downloads/ffmpeg/linux_64")
+    if [[ $DOWNLOAD_FFMPEG == "y" ]]; then
+      download_and_unpack "$CURRENT_DIRECTORY/downloads/ffmpeg/linux_64" "$FFMPEG_LINUX_64_DOWNLOAD_URL"
+    else
+      echo "ffmpeg for linux x64 has already been downloaded"
+    fi
+  else
+    echo "don't include ffmpeg according to the settings"
+  fi
 fi
 
 if [[ $BUILD_WIN_64 == "y" ]]; then
@@ -43,5 +61,23 @@ if [[ $BUILD_WIN_64 == "y" ]]; then
     download_and_unpack "$CURRENT_DIRECTORY/downloads/jdk/win_64" "$JDK_WIN_64_DOWNLOAD_URL"
   else
     echo "jdk for windows x64 has already been downloaded"
+  fi
+
+  DOWNLOAD_JAVAFX=$(need_to_download "$CURRENT_DIRECTORY/downloads/javafx/win")
+  if [[ $DOWNLOAD_JAVAFX == "y" ]]; then
+    download_and_unpack "$CURRENT_DIRECTORY/downloads/javafx/win" "$OPENJFX_WIN_DOWNLOAD_URL"
+  else
+    echo "javafx for windows has already been downloaded"
+  fi
+
+  if [[ $INCLUDE_FFMPEG == "y" ]]; then
+    DOWNLOAD_FFMPEG=$(need_to_download "$CURRENT_DIRECTORY/downloads/ffmpeg/win_64")
+    if [[ $DOWNLOAD_FFMPEG == "y" ]]; then
+      download_and_unpack "$CURRENT_DIRECTORY/downloads/ffmpeg/win_64" "$FFMPEG_WIN_64_DOWNLOAD_URL"
+    else
+      echo "ffmpeg for windows x64 has already been downloaded"
+    fi
+  else
+    echo "don't include ffmpeg according to the settings"
   fi
 fi
