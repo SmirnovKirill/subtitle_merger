5a529f04b14368b42ed052c0f0003a5ca845264b
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-11-14 01:03:15 +0300

    custom jre

--- build/make_custom_jre.sh
+++ build/make_custom_jre.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-function make_custom_jre {
-  rm $1 -rf
+set -e
 
-  jlink --module-path ../manual_downloads/jdk/$1/jmods \
+function make_custom_jre {
+  rm "$CURRENT_DIRECTORY/custom_jre/$1" -rf
+
+  if [[ $1 == "linux_64" ]]; then
+    JAVAFX_DIRECTORY_NAME="linux"
+  elif [[ $1 == "win_64" ]]; then
+    JAVAFX_DIRECTORY_NAME="win"
+  else
+    echo "unknown argument $1"
+    exit 1
+  fi
+
+  jlink --module-path "$CURRENT_DIRECTORY/downloads/jdk/$1/jmods:$CURRENT_DIRECTORY/downloads/javafx/$JAVAFX_DIRECTORY_NAME/lib" \
     --compress=2 \
     --no-man-pages \
-    --add-modules java.base,java.prefs \
-    --output $1
+    --add-modules java.base,java.prefs,javafx.base,javafx.controls,javafx.graphics,java.instrument,jdk.jdwp.agent \
+    --output "$CURRENT_DIRECTORY/custom_jre/$1"
 
-  echo "*" >> $1/.gitignore
+  create_git_ignore "$CURRENT_DIRECTORY/custom_jre/$1/.gitignore"
 }
 
 make_custom_jre "win_64"
