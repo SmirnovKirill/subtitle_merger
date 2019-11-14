ad1ff3e04c8b7e5ba7f9ef831affc9e9519c11b4
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-11-14 19:58:33 +0300

    linux run fix

--- src/main/for_build/run_scripts/run_linux.sh
+++ src/main/for_build/run_scripts/run_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 CURRENT_DIRECTORY="$(dirname "$0")"
-"$CURRENT_DIRECTORY/jre/bin/java" -jar "$CURRENT_DIRECTORY/*.jar"
+"$CURRENT_DIRECTORY/jre/bin/java" -jar "$CURRENT_DIRECTORY/"*.jar
