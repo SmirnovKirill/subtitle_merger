122f514a191b8e23228d5879071660205bd33e8e
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2019-12-02 11:40:51 +0300

    keep working on gui

--- src/main/java/kirill/subtitlesmerger/gui/MergeInVideosTabView.java
+++ src/main/java/kirill/subtitlesmerger/gui/MergeInVideosTabView.java
@@ -3,13 +3,29 @@ package kirill.subtitlesmerger.gui;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
+import kirill.subtitlesmerger.logic.Constants;
+import kirill.subtitlesmerger.logic.data.BriefFileInfo;
+import kirill.subtitlesmerger.logic.data.VideoFormat;
+import kirill.subtitlesmerger.logic.ffmpeg.Ffprobe;
+import kirill.subtitlesmerger.logic.ffmpeg.json.JsonFfprobeFileInfo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.joda.time.DateTime;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+
+import static kirill.subtitlesmerger.logic.data.BriefFileInfo.UnavailabilityReason.*;
+import static kirill.subtitlesmerger.logic.data.BriefFileInfo.UnavailabilityReason.NOT_ALLOWED_CONTAINER;
 
 class MergeInVideosTabView implements TabView {
     private static final String TAB_NAME = "Merge subtitles in videos";
@@ -47,6 +63,7 @@ class MergeInVideosTabView implements TabView {
         contentPane.getColumnConstraints().addAll(generateColumnConstraints());
 
         addRowMissingSettings(contentPane);
+        addRowFileTable(contentPane);
 
         return contentPane;
     }
@@ -78,6 +95,79 @@ class MergeInVideosTabView implements TabView {
         GridPane.setColumnSpan(missingSettingsBox, contentPane.getColumnCount());
     }
 
+    private void addRowFileTable(GridPane contentPane) {
+        TableView<TableFile> tableView = new TableView<>();
+
+        TableColumn<TableFile, String> fileNameColumn = new TableColumn<>("filename");
+        fileNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<TableFile, String> modificationDateColumn = new TableColumn<>("modification date");
+        modificationDateColumn.setCellValueFactory(new PropertyValueFactory<>("modificationTime"));
+
+        tableView.getColumns().add(fileNameColumn);
+        tableView.getColumns().add(modificationDateColumn);
+
+        tableView.getItems().addAll(getData());
+
+        contentPane.addRow(contentPane.getRowCount(), tableView);
+        GridPane.setColumnSpan(tableView, contentPane.getColumnCount());
+    }
+
+    private List<TableFile> getData() {
+        File directoryWithVideos = new File("/home/kirill");
+        File[] directoryFiles = directoryWithVideos.listFiles();
+
+        List<BriefFileInfo> briefFilesInfo = getBriefFilesInfo(directoryFiles);
+
+        List<TableFile> result = new ArrayList<>();
+
+        for (BriefFileInfo briefFileInfo : briefFilesInfo) {
+            result.add(new TableFile(briefFileInfo.getFile().getName(), new DateTime()));
+        }
+
+        return result;
+    }
+
+    private static List<BriefFileInfo> getBriefFilesInfo(File[] files) {
+        List<BriefFileInfo> result = new ArrayList<>();
+
+        for (File file : files) {
+            if (!file.isFile()) {
+                continue;
+            }
+
+            String extension = FilenameUtils.getExtension(file.getName());
+            if (StringUtils.isBlank(extension)) {
+                result.add(new BriefFileInfo(file, NO_EXTENSION, null, null));
+                continue;
+            }
+
+            if (!Constants.ALLOWED_VIDEO_EXTENSIONS.contains(extension.toLowerCase())) {
+                result.add(new BriefFileInfo(file, NOT_ALLOWED_EXTENSION, null, null));
+                continue;
+            }
+
+            String mimeType;
+            try {
+                mimeType = Files.probeContentType(file.toPath());
+            } catch (IOException e) {
+                result.add(new BriefFileInfo(file, FAILED_TO_GET_MIME_TYPE, null, null));
+                continue;
+            }
+
+            if (!Constants.ALLOWED_VIDEO_MIME_TYPES.contains(mimeType)) {
+                result.add(new BriefFileInfo(file, NOT_ALLOWED_MIME_TYPE, null, null));
+                continue;
+            }
+
+            result.add(
+                    new BriefFileInfo(file, null, null, null)
+            );
+        }
+
+        return result;
+    }
+
     @Override
     public String getTabName() {
         return TAB_NAME;
@@ -106,5 +196,13 @@ class MergeInVideosTabView implements TabView {
 
     void showRegularContent() {
         this.missingSettingsBox.setVisible(false);
+    }
+
+    @AllArgsConstructor
+    @Getter
+    private static class TableFile {
+        private String name;
+
+        private DateTime modificationTime;
     }
 }
