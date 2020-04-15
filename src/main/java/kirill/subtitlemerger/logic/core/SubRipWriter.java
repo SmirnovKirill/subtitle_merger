70fb00e47e1597c2222ba6b13a8064eca1a7c98c
Kirill Smirnov <smirnov.kirill.vladimirovich@gmail.com>
2020-04-15 21:57:27 +0300

    plain text

--- src/main/java/kirill/subtitlemerger/logic/core/SubRipWriter.java
+++ src/main/java/kirill/subtitlemerger/logic/core/SubRipWriter.java
@@ -5,7 +5,7 @@ import kirill.subtitlemerger.logic.core.entities.Subtitle;
 import kirill.subtitlemerger.logic.core.entities.Subtitles;
 
 public class SubRipWriter {
-    public static String toText(Subtitles subtitles) {
+    public static String toText(Subtitles subtitles, boolean plainText) {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < subtitles.getSubtitles().size(); i++) {
@@ -20,7 +20,11 @@ public class SubRipWriter {
             result.append(System.lineSeparator());
 
             for (int j = 0; j < subtitle.getLines().size(); j++) {
-                result.append(subtitle.getLines().get(j));
+                if (plainText) {
+                    result.append(getPlainText(subtitle.getLines().get(j)));
+                } else {
+                    result.append(subtitle.getLines().get(j));
+                }
 
                 if (j != subtitle.getLines().size() - 1 || i != subtitles.getSubtitles().size() - 1) {
                     result.append(System.lineSeparator());
@@ -33,5 +37,29 @@ public class SubRipWriter {
         }
 
         return result.toString();
+    }
+
+    /**
+     * This method removes HTML and ASS tags using regular expressions. I realize that it's not right to work with HTML
+     * using regular expressions and I could have used jsoup to clear tags but here are the arguments not to do it:
+     * 1) Subtitle text is not an html text, it's a regular text with html tags as far as I know. So for example string
+     * "<test" is correct in terms of subtitles but incorrect in terms of HTML because brackets that are not tags have
+     * to be encoded. So if I use jsoup it will simply remove the text "<test" completely instead of leaving it as is.
+     * 2) All not removed basic html-entities will be html encoded so I have to decode them back.
+     * 3) Even if I use jsoup there are no well-known libraries for removing ASS tags.
+     * 4) Regular expressions are faster than jsoup.
+     * 5) If it's possible not to use another library it's better to do so to reduce the jar's size.
+     * So I think in general for our purposes regular expressions will suffice.
+     */
+    private static String getPlainText(String text) {
+        String result = text;
+        if (text.contains("<") || text.contains(">")) {
+            result = result.replaceAll("<.+?>", "");
+        }
+        if (text.contains("{") || text.contains("}")) {
+            result = result.replaceAll("\\{.+?}", "");
+        }
+
+        return result;
     }
 }
