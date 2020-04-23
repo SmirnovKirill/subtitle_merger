a559974d91e01cce3e96b13c98a411beb0a40c03
Kirill Smirnov <smirnov.kirill.vladimirovich@gmail.com>
2020-04-23 14:23:16 +0300

    javafx 13->14

--- src/main/for_build/build_scripts/settings.sh
+++ src/main/for_build/build_scripts/settings.sh
@@ -6,7 +6,7 @@ BUILD_LINUX_64="y"
 BUILD_WIN_64="y"
 INCLUDE_FFMPEG="y"
 DOWNLOAD_EVERY_TIME="n" #for debug mostly
-CUSTOM_JRE_BASE_MODULES="java.base,java.prefs,java.management,java.naming" #java.management is required for log4j
+CUSTOM_JRE_BASE_MODULES="java.base,java.prefs,java.management" #java.management is required for log4j
 CUSTOM_JRE_LIB_MODULES="javafx.base,javafx.controls,javafx.graphics,javafx.fxml"
 CUSTOM_JRE_IDEA_MODULES="java.instrument,jdk.jdwp.agent"
 CUSTOM_JRE_ALL_MODULES="$CUSTOM_JRE_BASE_MODULES,$CUSTOM_JRE_LIB_MODULES,$CUSTOM_JRE_IDEA_MODULES"
@@ -16,8 +16,8 @@ JDK_LINUX_64_DOWNLOAD_URL="https://download.java.net/java/GA/jdk11/9/GPL/openjdk
 JDK_WIN_64_DOWNLOAD_URL="https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip"
 
 #https://gluonhq.com/products/javafx/
-OPENJFX_JMODS_LINUX_DOWNLOAD_URL="https://gluonhq.com/download/javafx-13.0.1-jmods-linux/"
-OPENJFX_JMODS_WIN_DOWNLOAD_URL="https://gluonhq.com/download/javafx-13.0.1-jmods-windows/"
+OPENJFX_JMODS_LINUX_DOWNLOAD_URL="https://gluonhq.com/download/javafx-14.0.1-jmods-linux/"
+OPENJFX_JMODS_WIN_DOWNLOAD_URL="https://gluonhq.com/download/javafx-14.0.1-jmods-windows/"
 
 #https://www.ffmpeg.org/download.html
 FFMPEG_LINUX_64_DOWNLOAD_URL="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
